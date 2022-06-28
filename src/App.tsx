fd7eea33e573b7c392fa6486858925a9569c3b02
Reshetnik-Eugenius <Eugenius@example.com>
2022-06-28 16:35:21 +0300

    add user - object

--- src/App.tsx
+++ src/App.tsx
@@ -15,34 +15,26 @@ function App() {
         { id: 2, name: 'Ervin Howell', email: 'Shanna@melissa.tv' },
         { id: 3, name: 'Clementine Bauch', email: 'Nathan@yesenia.net' }
     ]);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    const [user, setUser] = useState({name:'', email:''});
 
     const addNewUser = (event: ChangeEvent<HTMLInputElement>) => { 
         event.preventDefault();
-        const newUser = {
-            id: Date.now(),
-            name,
-            email
-        }
-        setUsers([...users, newUser]);
-        setName('');
-        setEmail('');
-        // console.log(newUser);
+        setUsers([...users, {...user, id: Date.now()}]);
+        setUser({name:'', email:''});
     }
 
     return (
         <div className="App">
             <form>
                 <PInput
-                    value={name}
-                    onChange={(event: ChangeEvent<HTMLInputElement>)  => setName(event.target.value)}
+                    value={user.name}
+                    onChange={(event: ChangeEvent<HTMLInputElement>)  => setUser({...user, name: event.target.value})}
                     type="text" 
                     placeholder="Name user" 
                 />
                 <PInput
-                    value={email}
-                    onChange={(event: ChangeEvent<HTMLInputElement>)  => setEmail(event.target.value)}
+                    value={user.email}
+                    onChange={(event: ChangeEvent<HTMLInputElement>)  => setUser({...user, email: event.target.value})}
                     type="text" 
                     placeholder="Email user" 
                 />
