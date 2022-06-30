18633629f8317d9e26fcd22443fa2efb88640ba0
Reshetnik-Eugenius <Eugenius@example.com>
2022-06-30 10:02:27 +0300

    animation project

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,21 +1,40 @@
-import React from 'react'
-import { IVal } from '../App';
-import UserItem from './UserItem';
+import React from "react";
+import { IVal } from "../App";
+import UserItem from "./UserItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-  const UserList = ({users, title, remove}: {users: IVal[], title: string, remove: any}) => {
-  if(!users.length){
+const UserList = ({
+    users,
+    title,
+    remove,
+}: {
+    users: IVal[];
+    title: string;
+    remove: any;
+}) => {
+    if (!users.length) {
+        return <h1 style={{ textAlign: "center" }}>Users not found!</h1>;
+    }
     return (
-      <h1 style={{textAlign: 'center'}}>Users not found!</h1>
-    )
-  }
-  return (
-    <div>
-        <h1 style={{textAlign: "center"}}>{title}</h1>
-            { users.map((user: any, index: number) =>
-                <UserItem remove={remove} number={index+1} user={user} key={user.id} />
-            )}
-    </div>
-  )
-}
+        <div>
+            <h1 style={{ textAlign: "center" }}>{title}</h1>
+            <TransitionGroup>
+                {users.map((user: any, index: number) => (
+                    <CSSTransition
+                        key={user.id}
+                        timeout={500}
+                        classNames="user"
+                    >
+                        <UserItem
+                            remove={remove}
+                            number={index + 1}
+                            user={user}
+                        />
+                        </CSSTransition>
+                ))}
+            </TransitionGroup>
+        </div>
+    );
+};
 
 export default UserList;
