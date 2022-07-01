5ce90ca46e89756b257e54fa986624456bcf9342
Reshetnik-Eugenius <Eugenius@example.com>
2022-07-01 15:27:39 +0300

    router - default, error links

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
 import About from './pages/About';
 import Users from './pages/Users';
+import Error from './pages/Error';
 import './styles/App.css'
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar/>
-            <Routes>
-                <Route path="about" element={<About />} />
-                <Route path="users" element={<Users />} />
-            </Routes>
+                <Routes>
+                    <Route path="/" element={<Users />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+
         </BrowserRouter>
     );
 }
