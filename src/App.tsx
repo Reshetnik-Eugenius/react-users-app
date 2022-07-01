aa9310409c62b28d7ab3a46e4e78050de7536146
Reshetnik-Eugenius <Eugenius@example.com>
2022-07-01 14:52:17 +0300

    add router - navbar click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Users from './pages/Users';
 import './styles/App.css'
@@ -6,6 +6,12 @@ import './styles/App.css'
 function App() {
     return (
         <BrowserRouter>
+            <div className='navbar'>
+                <div className="navbar__links">
+                    <Link to="/about">About</Link>
+                    <Link to="/users">Users</Link>
+                </div>
+            </div>
             <Routes>
                 <Route path="about" element={<About />} />
                 <Route path="users" element={<Users />} />
